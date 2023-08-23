dba5b959eb0419d0aaff75472197a89331dade45
Andrew Baker <142945177+AndrewDBaker@users.noreply.github.com>
2023-08-23 15:42:31 -0700

    wrote code for basic analysis, needs fine tunning. issue with column not being a list. easy

--- main.py
+++ main.py
@@ -5,21 +5,46 @@
 
 import pandas as pd
 import nltk
-nltk.download('averaged_perceptron_tagger')
 from nltk.sentiment import SentimentIntensityAnalyzer
 from textblob import TextBlob
 from collections import Counter
 import re
 
-# Download necessary NLTK data (if not already downloaded)
+nltk.download('averaged_perceptron_tagger')
 nltk.download('punkt')
 nltk.download('vader_lexicon')
 
- # Read the excel file
-amazon_reviews = pd.read_csv('amazon_reviews.csv')
 
-# Get the column you want
+amazon_reviews = pd.read_csv('amazon_reviews.csv')
 amazon_review_text_column = amazon_reviews['reviews.text']
 
-# Print the column
-print(column)
+
+def analyze_sentiment(amazon_review_text_column):
+    sia = SentimentIntensityAnalyzer()
+    sentiment_scores = sia.polarity_scores(amazon_review_text_column)
+    
+    if sentiment_scores['compound'] >= 0.05:
+        return 'Positive'
+    elif sentiment_scores['compound'] <= -0.05:
+        return 'Negative'
+    else:
+        return 'Neutral'
+
+def extract_keywords(amazon_review_text_column):
+    blob = TextBlob(amazon_review_text_column)
+    nouns = [word for word, tag in blob.tags if tag == 'NN']
+    return nouns
+
+def main():
+    for idx, review in enumerate(amazon_review_text_column, start=1):
+        print(f"Review {idx}: {amazon_review_text_column}")
+        sentiment = analyze_sentiment(amazon_review_text_column)
+        print(f"Sentiment: {sentiment}")
+        
+        keywords = extract_keywords(amazon_review_text_column)
+        print(f"Keywords: {', '.join(keywords)}")
+        
+        print("=" * 30)
+
+if __name__ == "__main__":
+    main()
