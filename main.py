0893bfe041515ccbb8d5ac95c1eb5c84ba66d865
Andrew Baker <142945177+AndrewDBaker@users.noreply.github.com>
2023-08-22 17:36:08 -0700

    installed pytorch and confirm it is working with  cuda. put on main py file

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 # Andrew Baker
 # Main python script
 # Goal of the script is to write an NLP analysis of a 
-# dataset found on kagglecom
+# dataset found on kaggle.com
+
+# using PyTorch 2.0.1 with CUDA
+
