f060b3a448f737dd83a39e99ec5aaac9c1d1e864
Andrew Baker <142945177+AndrewDBaker@users.noreply.github.com>
2023-08-23 15:33:50 -0700

    Added required libraries and wrote first bit of code for getting the data ready

--- main.py
+++ main.py
@@ -3,5 +3,23 @@
 # Goal of the script is to write an NLP analysis of a 
 # dataset found on kaggle.com
 
-# using PyTorch 2.0.1 with CUDA
+import pandas as pd
+import nltk
+nltk.download('averaged_perceptron_tagger')
+from nltk.sentiment import SentimentIntensityAnalyzer
+from textblob import TextBlob
+from collections import Counter
+import re
 
+# Download necessary NLTK data (if not already downloaded)
+nltk.download('punkt')
+nltk.download('vader_lexicon')
+
+ # Read the excel file
+amazon_reviews = pd.read_csv('amazon_reviews.csv')
+
+# Get the column you want
+amazon_review_text_column = amazon_reviews['reviews.text']
+
+# Print the column
+print(column)
